9a0237d621ae2d44d14c5fb8c8fc89269a2a2028
Irvin Cortez <irvincotez67@gmail.com>
2021-09-27 17:16:25 -0600

    Protected routes

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/{category}', [CategoryController::class, 'show']);
+    Route::post('/categories/create', [CategoryController::class, 'store']);
+    Route::put('/categories/update/{category}', [CategoryController::class, 'update']);
+    Route::delete('/categories/delete/{id}', [CategoryController::class, 'delete']);
 });
-
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/{category}', [CategoryController::class, 'show']);
-Route::post('/categories/create', [CategoryController::class, 'store']);
-Route::put('/categories/update/{category}', [CategoryController::class, 'update']);
-Route::delete('/categories/delete/{id}', [CategoryController::class, 'delete']);
 
 Route::post('/auth/register', [RegisterUserController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
